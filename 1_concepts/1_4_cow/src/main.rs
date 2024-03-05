8d12add7fda19cb2d140cf66a2fd5cb3494d2fd1
mishamuravynets <sodapumpkin@icloud.com>
2024-03-05 13:27:53 +0200

    1_4_cow

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Implement me!");
+use std::{borrow::Cow, env};
+
+const DEFAULT_CONF_PATH: &'static str = "/etc/app/app.conf";
+
+// I know that it should panic if `--conf` is empty, but this solution seems nicer to me,
+// allowing to just use the default one.
+fn extract_conf_path() -> Cow<'static, str> {
+    let env_var = env::vars().find(|e| e.0 == "APP_CONF").map(|e| e.1);
+
+    let env_arg_pos = env::args().into_iter().position(|a| a == "--conf");
+    let env_arg = env_arg_pos.and(env::args().into_iter().skip(env_arg_pos.unwrap()).next());
+
+    assert!(
+        !(env_arg_pos.is_some() && env_arg.is_none()),
+        "--conf can't be empty"
+    );
+
+    if let Some(s) = env_arg.or(env_var) {
+        Cow::from(s)
+    } else {
+        Cow::from(DEFAULT_CONF_PATH)
+    }
 }
+
+fn main() {}
