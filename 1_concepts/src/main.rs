ded4c52a8955526dcf291d53db52492dc5d3dab7
mishamuravynets <sodapumpkin@icloud.com>
2024-03-10 22:15:37 +0200

    append and prepend should work without race condition now

--- 1_concepts/src/main.rs
+++ 1_concepts/src/main.rs
@@ -1,9 +1,9 @@
-use std::ops::Deref;
 use std::sync::{Arc, RwLock};
 use std::thread;
-use std::thread::{sleep};
+use std::thread::sleep;
 use std::time::Duration;
 
+use arc_swap::access::Access;
 use arc_swap::ArcSwapOption;
 
 type ListRef<T> = ArcSwapOption<RwLock<Node<T>>>;
@@ -19,114 +19,6 @@ struct Node<T> {
 impl<T> Node<T> {
     fn default_list_ref() -> ListRef<T> {
         ArcSwapOption::empty()
-    }
-}
-
-struct DoublyLinkedList<T> {
-    head: ListRef<T>,
-    tail: ListRef<T>,
-}
-
-impl<T> DoublyLinkedList<T> {
-    fn new() -> Self {
-        Self {
-            head: Node::default_list_ref(),
-            tail: Node::default_list_ref(),
-        }
-    }
-
-    /// I couldn't modify p.prev for head without RwLock
-    fn prepend(&self, data: T) {
-        let new_head = Node {
-            data,
-            prev: None,
-            next: self.head.load_full(),
-        };
-
-        let prev_head = self.head.swap(Some(Arc::new(RwLock::new(new_head))));
-        match prev_head {
-            None => {}
-            Some(p) => {
-                p.write().unwrap().prev = self.head.load_full();
-            }
-        }
-
-        if self.tail.load().is_none() {
-            self.tail.store(self.head.load_full())
-        }
-    }
-
-    fn append(&self, data: T) {
-        let new_tail = Node {
-            data,
-            prev: self.tail.load_full(),
-            next: None,
-        };
-
-        let prev_tail = self.tail.swap(Some(Arc::new(RwLock::new(new_tail))));
-        match prev_tail {
-            None => {}
-            Some(p) => {
-                p.write().unwrap().next = self.tail.load_full();
-            }
-        }
-
-        if self.head.load().is_none() {
-            self.head.store(self.tail.load_full())
-        }
-    }
-
-    fn pop(&self) -> Option<T> {
-        if self.head.load().is_none() {
-            None
-        } else {
-            let new_head = self
-                .head
-                .load()
-                .deref()
-                .as_ref()
-                .unwrap()
-                .read()
-                .unwrap()
-                .next
-                .clone();
-            match &new_head {
-                None => {}
-                Some(p) => {
-                    p.write().unwrap().prev = None;
-                }
-            }
-            let old_head = self.head.swap(new_head).unwrap();
-
-            Arc::into_inner(old_head).map(|lock| lock.into_inner().unwrap().data)
-        }
-    }
-
-    fn into_iter(self) -> impl Iterator<Item = T>
-    where
-        T: Clone,
-    {
-        DoublyLinkedListIter {
-            curr: self.head.load_full(),
-        }
-    }
-
-    fn into_iter_rev(self) -> impl Iterator<Item = T>
-    where
-        T: Clone,
-    {
-        DoublyLinkedListIterReversed {
-            curr: self.tail.load_full(),
-        }
-    }
-}
-
-impl<T> Clone for DoublyLinkedList<T> {
-    fn clone(&self) -> Self {
-        DoublyLinkedList {
-            head: ArcSwapOption::from(self.head.load_full()),
-            tail: ArcSwapOption::from(self.tail.load_full()),
-        }
     }
 }
 
@@ -159,7 +51,8 @@ struct DoublyLinkedListIterReversed<T> {
 }
 
 impl<T: Clone> Iterator for DoublyLinkedListIterReversed<T> {
-    type Item = T;fn next(&mut self) -> Option<Self::Item> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
         let mut next = None;
         let data = match &self.curr {
             None => None,
@@ -176,31 +69,171 @@ impl<T: Clone> Iterator for DoublyLinkedListIterReversed<T> {
     }
 }
 
+struct DoublyLinkedList<T> {
+    head: ListRef<T>,
+    tail: ListRef<T>,
+}
+
+impl<T> DoublyLinkedList<T> {
+    fn new() -> Self {
+        Self {
+            head: Node::default_list_ref(),
+            tail: Node::default_list_ref(),
+        }
+    }
+
+    /// I couldn't modify p.prev for head without RwLock
+    fn prepend(&self, data: T) {
+        let new_head = ArcSwapOption::from_pointee(RwLock::new(Node {
+            data,
+            prev: None,
+            next: None,
+        }));
+        
+        if self.tail.load().is_none() {
+            self.tail.rcu(|rcu| {
+                if rcu.is_none() {
+                    new_head.load_full()
+                } else {
+                    rcu.clone()
+                }
+            });
+        }
+
+        self.head.rcu(|rcu| {
+            match new_head.load().clone() {
+                None => {}
+                Some(new) => {
+                    new.write().unwrap().next = rcu.clone();
+                }
+            }
+
+            match rcu.clone() {
+                None => {}
+                Some(p) => {
+                    p.write().unwrap().prev = new_head.load_full();
+                }
+            }
+
+            new_head.load_full()
+        });
+    }
+
+    fn append(&self, data: T) {
+        let new_tail = ArcSwapOption::from_pointee(RwLock::new(Node {
+            data,
+            prev: None,
+            next: None,
+        }));
+
+        if self.head.load().is_none() {
+            self.head.rcu(|rcu| {
+                if rcu.is_none() {
+                    new_tail.load_full()
+                } else {
+                    rcu.clone()
+                }
+            });
+        }
+
+        self.tail.rcu(|rcu| {
+            match new_tail.load().clone() {
+                None => {}
+                Some(new) => {
+                    new.write().unwrap().prev = rcu.clone();
+                }
+            }
+
+            match rcu.clone() {
+                None => {}
+                Some(p) => {
+                    p.write().unwrap().next = new_tail.load_full();
+                }
+            }
+
+            new_tail.load_full()
+        });
+    }
+
+    fn pop(&self) -> Option<T> {
+        if self.head.load().is_none() || self.tail.load().is_none() {
+            None
+        } else {
+            let old_head = self.head.rcu(|rcu| {
+                let next = rcu.clone().unwrap().read().unwrap().next.clone();
+                match next.clone() {
+                    None => {}
+                    Some(p) => {
+                        p.write().unwrap().prev = None;
+                    }
+                }
+
+                next
+            });
+
+            Arc::into_inner(old_head.unwrap()).map(|lock| lock.into_inner().unwrap().data)
+        }
+    }
+
+    fn into_iter(self) -> impl Iterator<Item=T>
+        where
+            T: Clone,
+    {
+        DoublyLinkedListIter {
+            curr: self.head.load_full(),
+        }
+    }
+
+    fn into_iter_rev(self) -> impl Iterator<Item=T>
+        where
+            T: Clone,
+    {
+        DoublyLinkedListIterReversed {
+            curr: self.tail.load_full(),
+        }
+    }
+}
+
+impl<T> Clone for DoublyLinkedList<T> {
+    fn clone(&self) -> Self {
+        DoublyLinkedList {
+            head: ArcSwapOption::from(self.head.load_full()),
+            tail: ArcSwapOption::from(self.tail.load_full()),
+        }
+    }
+}
+
 fn main() {
     let l = DoublyLinkedList::new();
 
     thread::scope(|s| {
         s.spawn(|| {
-            //sleep(Duration::from_nanos(6));
-            for i in 11..20 {
-                l.prepend(i);
-            }
-        });
-
-        s.spawn(|| {
-            for i in 1..10 {
-                //sleep(Duration::from_nanos(5));
+            for i in 8..15 {
                 l.append(i);
             }
         });
 
         s.spawn(|| {
-            for _ in 1..10 {
-                sleep(Duration::from_nanos(1));
-                for n in l.clone().into_iter_rev() {
-                    print!("{}, ", n);
-                }
-                print!("\n\n");
+            for i in (1..8).rev() {
+                l.prepend(i);
+            }
+        });
+
+        s.spawn(|| {
+            sleep(Duration::from_nanos(500));
+            for _ in 1..4 {
+                println!("{:?}", l.pop());
+            }
+        });
+
+        s.spawn(|| {
+            sleep(Duration::from_millis(1000));
+            for n in l.clone().into_iter() {
+                print!("{}, ", n);
+            }
+            print!("\n\n\n");
+            for n in l.clone().into_iter_rev() {
+                print!("{}, ", n);
             }
         });
     });
