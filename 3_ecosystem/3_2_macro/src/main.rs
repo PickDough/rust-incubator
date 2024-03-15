9d6c1e5d8e78b01c552cd125347088168d3c6fca
mishamuravynets <sodapumpkin@icloud.com>
2024-03-15 14:32:57 +0200

    3_2_macro

--- 3_ecosystem/3_2_macro/src/main.rs
+++ 3_ecosystem/3_2_macro/src/main.rs
@@ -1,3 +1,25 @@
+use btree_proc_macro::btree_proc;
+use std::collections::BTreeMap;
+
+macro_rules! btree_decl {
+    ($(($k:expr, $v:expr)),*) => {
+        {
+            let mut b_tree = BTreeMap::new();
+            $(
+                b_tree.insert($k, $v);
+            )*
+
+            b_tree
+        }
+    };
+}
+
 fn main() {
     println!("Implement me!");
+
+    let b = btree_decl![("1", 2), ("3", 4)];
+
+    let x = btree_proc!((1, 2), (3, 5));
+
+    println!("{:?}", x);
 }
