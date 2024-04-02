23ff201133d9893959787a9cf4e1afd8a92564fa
mishamuravynets <sodapumpkin@icloud.com>
2024-04-02 17:03:37 +0300

    4_2_http

---
+++ 4_backend/4_2_http/src/commands/mod.rs
@@ -0,0 +1,14 @@
+#[allow(async_fn_in_trait)]
+use std::fmt::Debug;
+
+pub mod clap;
+pub mod roles;
+pub mod users;
+
+pub trait Command {}
+
+pub trait CommandHandler<C: Command> {
+    type Output: Debug;
+
+    async fn handle(&self, command: C) -> Self::Output;
+}
